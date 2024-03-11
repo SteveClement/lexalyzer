c004e32329481510570a8724133961a98c1db4bf
Steve Clement <steve@localhost.lu>
2024-03-11 09:47:35 +0100

    chg: [py] Linted code removed some superflous code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 Main code for the YouTube Audio analyzer.
 """
+
 #!/usr/bin/env python
 import wave
 import os
@@ -11,39 +12,47 @@ from pyAudioAnalysis import audioSegmentation as aS
 
 try:
     from keys import api_key
-    youtube = build('youtube', 'v3', developerKey=api_key)
-except IOError as e:
-    print(f"An error occurred: {e}")
 
-debug=False
+    youtube = build("youtube", "v3", developerKey=api_key)
+except IOError as error:
+    print(f"An error occurred: {error}")
+
+debug = False
+
 
 def detect_speakers_and_durations(file_path):
     """
-        Detects speakers in an audio file and calculates their durations.
+    Detects speakers in an audio file and calculates their durations.
 
-        Returns:
-            dict: A dictionary containing the durations of each speaker in a more readable format.
-                The keys are in the format "Speaker <speaker_number>" and the values are the durations in seconds.
+    Returns:
+        dict: A dictionary containing the durations of each speaker in a more readable format.
+            The keys are in the format "Speaker <speaker_number>" and the values 
+            are the durations in seconds.
     """
     # Perform speaker diarization
-    [flagsInd, classesAll, acc] = aS.speaker_diarization(file_path, 0, plot_res=False)
+    [flags_ind, classesAll, acc] = aS.speaker_diarization(file_path, 0, plot_res=False)
 
     # Initialize a dictionary to hold speaker durations
     speaker_durations = {}
 
-    # Calculate the duration of each segment assuming a fixed window step in seconds (e.g., 0.2s as default in pyAudioAnalysis)
+    # Calculate the duration of each segment assuming a fixed window step in seconds
+    # (e.g., 0.2s as default in pyAudioAnalysis)
     window_step = 0.2
 
-    for speaker_label in flagsInd:
+    for speaker_label in flags_ind:
         if speaker_label not in speaker_durations:
             speaker_durations[speaker_label] = window_step
         else:
             speaker_durations[speaker_label] += window_step
 
     # Convert speaker labels to a more readable format
-    readable_speaker_durations = {f"Speaker {int(speaker) + 1}": duration for speaker, duration in speaker_durations.items()}
+    readable_speaker_durations = {
+        f"Speaker {int(speaker) + 1}": duration
+        for speaker, duration in speaker_durations.items()
+    }
 
     return readable_speaker_durations
+
 
 def video_already_downloaded(ydl_opts, video_url):
     """Check if the video has already been downloaded based on yt-dlp options."""
@@ -51,11 +60,15 @@ def video_already_downloaded(ydl_opts, video_url):
         # Fetch video info without downloading
         info_dict = ydl.extract_info(video_url, download=False)
         filename = ydl.prepare_filename(info_dict)
-        base = os.path.splitext(filename)[0] 
-        filename = base + '.wav'
-        if debug: print(f"Checking if {filename} exists: {os.path.exists(os.path.join(filename))}")
+        base = os.path.splitext(filename)[0]
+        filename = base + ".wav"
+        if debug:
+            print(
+                f"Checking if {filename} exists: {os.path.exists(os.path.join(filename))}"
+            )
         # Check if the file exists in the output directory
         return [os.path.exists(os.path.join(filename)), filename]
+
 
 def get_channel_videos(channel_id):
     """
@@ -69,29 +82,39 @@ def get_channel_videos(channel_id):
     """
     # Get the upload playlist ID
     try:
-        res = youtube.channels().list(id=channel_id, part='contentDetails').execute()
-        if res['pageInfo']['totalResults'] == 0:
+        res = youtube.channels().list(id=channel_id, part="contentDetails").execute()
+        if res["pageInfo"]["totalResults"] == 0:
             print(f"Channel ID {channel_id} not found.")
             return False
-    except Exception as e:
+    except ValueError as e:
         print(f"An error occurred: {e}")
 
-    playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+    playlist_id = res["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
     videos = []
     next_page_token = None
 
     while True:
-        res = youtube.playlistItems().list(playlistId=playlist_id, part='snippet', maxResults=50, pageToken=next_page_token).execute()
-        videos += res['items']
-        next_page_token = res.get('nextPageToken')
+        res = (
+            youtube.playlistItems()
+            .list(
+                playlistId=playlist_id,
+                part="snippet",
+                maxResults=50,
+                pageToken=next_page_token,
+            )
+            .execute()
+        )
+        videos += res["items"]
+        next_page_token = res.get("nextPageToken")
 
         if next_page_token is None:
             break
 
     return videos
 
-def get_video(videos, mode='id'):
+
+def get_video(videos, mode="id"):
     """
     Prints the duration of each video in the given list of videos.
 
@@ -101,23 +124,29 @@ def get_video(videos, mode='id'):
     Returns:
         None
     """
-    if mode == 'duration':
+    if mode == "duration":
         for video in videos:
-            video_id = video['snippet']['resourceId']['videoId']
-            video_details = youtube.videos().list(id=video_id, part='contentDetails').execute()
-            duration = video_details['items'][0]['contentDetails']['duration']
+            video_id = video["snippet"]["resourceId"]["videoId"]
+            video_details = (
+                youtube.videos().list(id=video_id, part="contentDetails").execute()
+            )
+            duration = video_details["items"][0]["contentDetails"]["duration"]
             duration_in_seconds = isodate.parse_duration(duration).total_seconds()
-            print(f"Title: {video['snippet']['title']}, Duration: {duration_in_seconds} seconds, ID: {video_id}")
-    if mode == 'id':
+            print(
+                f"Title: {video['snippet']['title']}, \
+                Duration: {duration_in_seconds} seconds, ID: {video_id}"
+            )
+    if mode == "id":
         video_ids = []
         for video in videos:
-            video_id = video['snippet']['resourceId']['videoId']
+            video_id = video["snippet"]["resourceId"]["videoId"]
             video_ids.append(video_id)
-            if debug: print(f"Title: {video['snippet']['title']}, ID: {video_id}")
+            if debug:
+                print(f"Title: {video['snippet']['title']}, ID: {video_id}")
         return video_ids
 
 
-def download_audio(video_url, output_path='output/'):
+def download_audio(video_url, output_path="output/"):
     """Downloads audio from the given video URL using yt-dlp.
 
     Args:
@@ -125,30 +154,34 @@ def download_audio(video_url, output_path='output/'):
     output_path (str): Directory to save the downloaded audio file.
     """
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'quiet': True,  # Suppresses most of the console output
-        'no_warnings': True,  # Suppresses warning messages
-        'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'wav',
-                'preferredquality': '192'
-                }],
-                'postprocessor_args': [
-                '-ac', '1'  # This line ensures the audio is mono
-                ],
-                'outtmpl': os.path.join(output_path, '%(title)s.%(ext)s'),
+        "format": "bestaudio/best",
+        "quiet": True,  # Suppresses most of the console output
+        "no_warnings": True,  # Suppresses warning messages
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "wav",
+                "preferredquality": "192",
+            }
+        ],
+        "postprocessor_args": ["-ac", "1"],  # This line ensures the audio is mono
+        "outtmpl": os.path.join(output_path, "%(title)s.%(ext)s"),
     }
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    alreadyDown, filename = video_already_downloaded(ydl_opts, video_url)
-    if alreadyDown:
-        if debug: print("Video has already been downloaded. Skipping download.")
+    already_down, filename = video_already_downloaded(ydl_opts, video_url)
+    if already_down:
+        if debug:
+            print("Video has already been downloaded. Skipping download.")
         return filename
 
     with YoutubeDL(ydl_opts) as ydl:
-        res = ydl.download([video_url]) # res seems to be the exit code of the download process
+        res = ydl.download(
+            [video_url]
+        )  # res seems to be the exit code of the download process
+        return None
 
 
 def symlink_audio(source, destination):
@@ -185,46 +218,57 @@ def analyze_wav(file_path):
 
     """
     # Open the WAV file
-    with wave.open(file_path, 'rb') as wav_file:
+    with wave.open(file_path, "rb") as wav_file:
         # Extract audio parameters
-        n_channels = wav_file.getnchannels() # Number of channels
-        sample_width = wav_file.getsampwidth() # Sample width in bytes
-        frame_rate = wav_file.getframerate() # Sample rate
-        n_frames = wav_file.getnframes() # Number of frames
+        n_channels = wav_file.getnchannels()  # Number of channels
+        sample_width = wav_file.getsampwidth()  # Sample width in bytes
+        frame_rate = wav_file.getframerate()  # Sample rate
+        n_frames = wav_file.getnframes()  # Number of frames
 
         # Calculate duration in seconds
         duration = n_frames / frame_rate
 
         # Calculate bit rate
-        bit_rate = frame_rate * sample_width * 8 * n_channels # in bits per second
+        bit_rate = frame_rate * sample_width * 8 * n_channels  # in bits per second
 
         # Format details
         format_details = {
-            'Number of Channels': n_channels,
-            'Sample Width (bytes)': sample_width,
-            'Frame Rate / Sample Rate (Hz)': frame_rate,
-            'Number of Frames': n_frames,
-            'Duration (seconds)': duration,
+            "Number of Channels": n_channels,
+            "Sample Width (bytes)": sample_width,
+            "Frame Rate / Sample Rate (Hz)": frame_rate,
+            "Number of Frames": n_frames,
+            "Duration (seconds)": duration,
         }
 
         return frame_rate, bit_rate, format_details
 
-def search_channel_by_name(channel_name):
-    search_response = youtube.search().list(
-        q=channel_name,
-        type='channel',
-        part='id,snippet',
-        maxResults=1
-    ).execute()
 
-    if search_response['items']:
-        channel_id = search_response['items'][0]['id']['channelId']
-        channel_title = search_response['items'][0]['snippet']['title']
-        if debug: print(f"Channel ID: {channel_id}, Channel Title: {channel_title}")
+def search_channel_by_name(channel_name):
+    """
+    Searches for a YouTube channel by name.
+
+    Args:
+        channel_name (str): The name of the channel to search for.
+
+    Returns:
+        str or None: The channel ID if found, None otherwise.
+    """
+    search_response = (
+        youtube.search()
+        .list(q=channel_name, type="channel", part="id,snippet", maxResults=1)
+        .execute()
+    )
+
+    if search_response["items"]:
+        channel_id = search_response["items"][0]["id"]["channelId"]
+        channel_title = search_response["items"][0]["snippet"]["title"]
+        if debug:
+            print(f"Channel ID: {channel_id}, Channel Title: {channel_title}")
         return channel_id
     else:
         print("Channel not found.")
         return None
+
 
 ##videos = get_channel_videos(channel_id)
 ##if not videos:
@@ -233,32 +277,33 @@ def search_channel_by_name(channel_name):
 ##    print_video_durations(videos)
 
 # Example usage:
-#file_path = 'converted.wav'
-#sample_rate, bit_rate, format_details = analyze_wav(file_path)
-#print(f"Sample Rate: {sample_rate} Hz")
-#print(f"Bit Rate: {bit_rate} bits/s")
-#print("Format Details:")
-#for key, value in format_details.items():
+# file_path = 'converted.wav'
+# sample_rate, bit_rate, format_details = analyze_wav(file_path)
+# print(f"Sample Rate: {sample_rate} Hz")
+# print(f"Bit Rate: {bit_rate} bits/s")
+# print("Format Details:")
+# for key, value in format_details.items():
 #    print(f"{key}: {value}")
 
-if __name__ == '__main__':
-    #if len(sys.argv) != 2:
+if __name__ == "__main__":
+    # if len(sys.argv) != 2:
     #    print("Usage: python script.py <YouTube Video URL>")
     #    sys.exit(1)
 
     # video_url = sys.argv[1]
-    #video_url = 'https://www.youtube.com/watch?v=jrIJa2-niBM'
-    #file_path = download_audio(video_url)
-    #channel_name = 'Klonter77'  # This should be the name or a part of the custom URL
+    # video_url = 'https://www.youtube.com/watch?v=jrIJa2-niBM'
 
-    channel_name = '@LexFridman'  # Replace 'CHANNEL_ID' with the actual channel ID
-    channel_id = search_channel_by_name(channel_name)
-    videos = get_video(get_channel_videos(search_channel_by_name(channel_name)), mode='id')
+    CHANNEL_NAME = "@LexFridman"  # This should be the name or a part of the custom URL
+    OUTPUT_PATH = "output/"
+    channel_id = search_channel_by_name(CHANNEL_NAME)
+    videos = get_video(
+        get_channel_videos(search_channel_by_name(CHANNEL_NAME)), mode="id"
+    )
 
-    for id in videos:
-        download_audio(id, output_path='output/LexFridman/')    
+    for vid_id in videos:
+        download_audio(vid_id, output_path=f"{OUTPUT_PATH}+{CHANNEL_NAME}/")
 
-    #file_path="output/test.wav"
-    #speaker_durations = detect_speakers_and_durations(file_path)
-    #for speaker, duration in speaker_durations.items():
+    # file_path="output/test.wav"
+    # speaker_durations = detect_speakers_and_durations(file_path)
+    # for speaker, duration in speaker_durations.items():
     #    print(f"{speaker} talked for {duration:.2f} seconds")
